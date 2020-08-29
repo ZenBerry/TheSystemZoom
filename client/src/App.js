676f9e872da58f754ad5ed725e8e5d643821723c
ZenBerry <zenberry.music@gmail.com>
2020-08-29 10:55:22 +0300

    And another

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,15 @@
 import React, { useState, useRef, useEffect } from "react";
 import io from "socket.io-client";
-const socket = io("http://localhost:5000");
+
+
+var connectionOptions =  {
+            "force new connection" : true,
+            "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
+            "timeout" : 10000,                  //before connect_error and connect_timeout are emitted.
+            "transports" : ["websocket"]
+        };
+
+const socket = io("http://localhost:5000", connectionOptions);
 
 // var io = io_init('http://localhost:5000', {transports: ['websocket', 'polling', 'flashsocket']});
 
