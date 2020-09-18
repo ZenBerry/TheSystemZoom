4550aedabcb8b87062c251a113fe37229fb04acd
ZenBerry <zenberry.music@gmail.com>
2020-09-18 19:05:37 +0300

    Healthy little progress. Keep going!

--- client/src/App.js
+++ client/src/App.js
@@ -1,72 +1,16 @@
-// import React, { useState, useRef, useEffect } from "react";
-// import InfiniteViewer from "react-infinite-viewer";
-
-// function App () {
-//   return (
-
-//     <div> 
-
-
-
-
-    
-//    <InfiniteViewer
-//        className="viewer"
-//        margin={1}
-//        threshold={1}
-//        rangeX={[1000, 1000]}
-//        rangeY={[1000, 1000]}
-//        onScroll={e => {
-//            console.log(e);
-//        }}
-//        >
-//        <div style={{height: '100vh'}} className="viewport">
-//        Paul and me
-
-
-
-
-//        //insert moveable here
-
-
-
-
-//        </div>
-//    </InfiniteViewer>
-
-
-
-
-
-
-
-
-
-
-
-//     </div>
-
-//     )
-
-//   };
-
-//   export default App;
-
-//everything below has to be uncommented haha
-
 import React, { useState, useRef, useEffect } from "react";
 import io from "socket.io-client";
 import Draggable from 'react-draggable'
 import Moveable from './components/Moveable' 
 
+import useMousePosition from './components/useMousePosition' 
+
+
+
 import InfiniteViewer from "react-infinite-viewer"; //infinite scroll
 
-import panzoom from "panzoom"
 
 import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock'; //default scroll lock
-
-import { MapInteractionCSS } from 'react-map-interaction'; //zoom
-import { MapInteraction } from 'react-map-interaction'; //zoom
 
 var connectionOptions =  {
             "force new connection" : true,
@@ -78,7 +22,6 @@ var connectionOptions =  {
 // const socket = io("https://finance-test-websockets.herokuapp.com/", connectionOptions); //for running online
 const socket = io("http://localhost:5000", connectionOptions); //for running locally
 
-// var io = io_init('http://localhost:5000', {transports: ['websocket', 'polling', 'flashsocket']});
 
 
 function App () {
@@ -96,6 +39,8 @@ function App () {
 
   const [formValue, setFormValue] = useState("")
 
+ 
+
   //Drag vars 
 
  
@@ -109,13 +54,11 @@ function App () {
 
   const [positions, setPositions] = useState([])
 
+  //Zoom cars
+
 
  const [zoom, setZoom] = useState(1)
 
- const [zoomValue, setZoomValue] = useState({
-         scale: 1,
-         translation: { x: 500, y: 500 }
-       })
 
  const [pinchScroll, setPinchScroll] = useState(false)
 
@@ -124,21 +67,27 @@ function App () {
 
  const [isZoomingIn, setIsZoomingIn] = useState(null)
 
- const [cursorX, setCursorX] = useState(0)
- const [cursorY, setCursorY] = useState(0)
+ const { x, y } = useMousePosition(); //get mouse position 
+ const hasMovedCursor = typeof x === "number" && typeof y === "number"; //mouse position again
+
+ const [cursorX, setCursorX] = useState(x) //workaround, should be deleted later on
+ const [cursorY, setCursorY] = useState(y)
+
+ const [startPanX, setStartPanX] = useState(null)
+ const [startPanY, setStartPanY] = useState(null) //course "programming panning and zooming, 9:32" We have to update those vars only onPinch
 
  const ref = useRef(null);
 
  useEffect(() => {
-     console.log('width', ref.current ? ref.current.getBoundingClientRect().width/2 : 0);
-     console.log('height', ref.current ? ref.current.getBoundingClientRect().height/2 : 0);
+     console.log('cursor from the world x', ref.current ? x-ref.current.getBoundingClientRect().x : 0);
+     console.log('cursor from the world y', ref.current ? y-ref.current.getBoundingClientRect().y : 0);
 
      setPinchOffsetY(ref.current.getBoundingClientRect().height)
      setPinchOffsetX(ref.current.getBoundingClientRect().width)
 
      // ref.current.offsetWidth
 
-   }, []);
+   }, [x, y]);
 
 
 
@@ -187,23 +136,11 @@ function App () {
 
         });
 
-
-
- 
-
-
-
-
-
-
   }, []);
 
   function handleClick() {
 
-   
-
-
-
+  
   }
 
   function handleSubmit(e : any) {
@@ -268,45 +205,13 @@ function App () {
     socket.emit("addMoveables", moveables+1, e.clientX, e.clientY-33) //WE HAVE TO MAKE IT BETTER
      
 
-     // if(document.selection && document.selection.empty) { //DOUBLE CLICK SELECTION PREVENTION 0
-     //     document.selection.empty();
-     // } else if(window.getSelection) {
-     //     var sel = window.getSelection();
-     //     sel.removeAllRanges();
-     // } 
-  }
-
-
-  // console.log("MY SOCKET FROM APP",mySocket)
 
   
 
 
-
+}
   document.ondblclick = (e) => handleMoveableAddition(e);
 
-
-//   var element = document.querySelector('#root')
-//   panzoom(element, {
-//   beforeMouseDown: function(e) {
-//     // allow mouse-down panning only if altKey is down. Otherwise - ignore
-//     var shouldIgnore = !e.shiftKey;
-//     return shouldIgnore;
-//   },
-//   beforeWheel: function(e) {
-//       // allow wheel-zoom only if altKey is down. Otherwise - ignore
-//       var shouldIgnore = !e.altKey;
-//       return shouldIgnore;
-//     },
-//     onTouch: function(e) {
-//       // `e` - is current touch event.
-
-//       return false; // tells the library to not preventDefault.
-//     }
-
-
-
-// })
 
 
 
@@ -326,8 +231,17 @@ function App () {
    Offset X {pinchOffsetX} <br/>
    Offset Y {pinchOffsetY} <br/>
    Zoom {zoom}  <br/>
-   Cursor X {cursorX}  <br/>
-   Cursor Y {cursorY}  <br/>
+   {hasMovedCursor
+           ? (`Your cursor is at ${x}, ${y}.`)
+           : "Move your mouse around."} <br/>
+
+   cursorX {cursorX} <br/>
+   cursorY {cursorY} <br/>
+
+
+
+
+
 
    </div>
 
@@ -349,9 +263,6 @@ function App () {
 
 
 
-    {/*    setPinchOffsetX(e.scrollLeft)
-        setPinchOffsetY(e.scrollTop)*/}
-
 
 
         }}
@@ -368,21 +279,7 @@ function App () {
 
             setIsZoomingIn(false)
 
-{/*            if (zoom > 0.1) {
 
-              console.log('zoom is > 0.1')
-              setPinchOffsetX(prev => prev-10)
-              setPinchOffsetY(prev => prev-10)  
-
-
-
-            }*/}
-
-
-
-
-         {/*   setPinchOffsetX(prev => prev-10)
-            setPinchOffsetY(prev => prev-10)*/}
 
 
 
@@ -391,28 +288,6 @@ function App () {
 
             console.log('zooming in') ;
 
-{/*            if (zoom > 0.1) {
-
-              console.log('zoom is > 0.1')
-              setPinchOffsetX(prev => prev+10)
-              setPinchOffsetY(prev => prev+10)  
-
-              
-
-            }*/}
-
-
-{/*            if (e.inputEvent.clientX > pinchOffsetX ) {
-
-            setPinchOffsetX(prev => prev+10)
-              
-            } 
-
-            if (e.inputEvent.clientY > pinchOffsetY ) {
-
-            setPinchOffsetY(prev => prev+10)
-              
-            } */}
 
 
             setIsZoomingIn(true) 
@@ -429,15 +304,8 @@ function App () {
 
 
           )  
-         setZoomValue({
-         scale: zoomValue.scale*e.zoom*e.scale,
-         translation: { x: 0, y: 0 }
-       })
 
-         {/*console.log("PINCHING!", e.inputEvent.clientX)*/}
-{/*
-         setPinchOffsetX(e.inputEvent.clientX)
-         setPinchOffsetY(e.inputEvent.clientY)*/}
+
 
 
 
@@ -452,7 +320,7 @@ function App () {
 
         <div  ref={ref} style={{transform:'scale('+zoom+')' +  'translateX('+ (pinchOffsetX*zoom/2- cursorX) +'px)' + 'translateY('+ (pinchOffsetY*zoom/2 -cursorY) +'px)' , backgroundColor: "white", height: '100vh'}}>
 
-        {/*PAUSED HERE 14 SEP*/}
+
 
 
         { moveables > -1 && ( [...Array(moveables)].map((e, i) =>  <span style={{position: 'absolute', top:0, left: 0}}>  <Moveable  socket={socket} mySocket={mySocket} id = {i}  x={moveableInitX} y={moveableInitY}>  </Moveable> </span>) )}
@@ -470,7 +338,7 @@ function App () {
 
   
 
-  {/* {  moveables > -1 && ( [...Array(moveables)].map((e, i) =>  <span style={{position: 'absolute', top:0, left: 0}}>  <Moveable  socket={socket} mySocket={mySocket} id = {i}  x={moveableInitX} y={moveableInitY}>  </Moveable> </span>) )}*/}
+  
 
 
 
