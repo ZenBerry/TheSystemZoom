b9572839f26c3ea524a0177f26d61bda2da53517
ZenBerry <zenberry.music@gmail.com>
2020-10-15 19:51:34 +0300

    Formatted for Jay
    It zooms using desktop pinch gesture, but only if you:
    
    1. zoom in with a weird bug
    2. unzoom
    3. Zoom in again (the point becomes displaced though)

--- client/src/App.js
+++ client/src/App.js
@@ -1,90 +1,82 @@
-import React, { useState, useRef, useEffect } from "react";
-import io from "socket.io-client";
-import Draggable from 'react-draggable'
-import Moveable from './components/Moveable' 
+ // Imports
 
-import useMousePosition from './components/useMousePosition' 
+		import React, { useState, useRef, useEffect } from "react";
+		import io from "socket.io-client";
+		import Draggable from 'react-draggable'
+		import Moveable from './components/Moveable' 
+		import useMousePosition from './components/useMousePosition' 
+		import InfiniteViewer from "react-infinite-viewer"; //infinite scroll
+		import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock'; //default scroll lock
 
+ // Connect to the websocket
+		var connectionOptions =  {
+		            "force new connection" : true,
+		            "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
+		            "timeout" : 10000,                  //before connect_error and connect_timeout are emitted.
+		            "transports" : ["websocket"]
+		        };
 
+		// const socket = io("https://finance-test-websockets.herokuapp.com/", connectionOptions); //for running online
+		const socket = io("http://localhost:5000", connectionOptions); //for running locally
 
-import InfiniteViewer from "react-infinite-viewer"; //infinite scroll
+ // The var to create a canvas (it's used to draw a line)
 
-
-import { disableBodyScroll, enableBodyScroll, clearAllBodyScrollLocks } from 'body-scroll-lock'; //default scroll lock
-
-var connectionOptions =  {
-            "force new connection" : true,
-            "reconnectionAttempts": "Infinity", //avoid having user reconnect manually in order to prevent dead clients after a server restart
-            "timeout" : 10000,                  //before connect_error and connect_timeout are emitted.
-            "transports" : ["websocket"]
-        };
-
-// const socket = io("https://finance-test-websockets.herokuapp.com/", connectionOptions); //for running online
-const socket = io("http://localhost:5000", connectionOptions); //for running locally
-
-let ctx = null;
+	 	 let ctx = null; 	
 
 function App () {
 
-  const [sum, setSum] = useState(-1)
+ // Zoom and pan vars, forJay: those are related to the problem
 
-  const [response,setResponse] = useState('Loading...')
-  const [passwords,setPasswords] = useState([])
-  const [myVar,setMyVar] = useState("loading...")
+		 const [zoom, setZoom] = useState(1) //initial scale
 
-  const [test,setTest] = useState("TEST")
+		 const [isZoomingIn, setIsZoomingIn] = useState(null)
 
-  const [value,setValue] = useState(null)
-  const [newData,setNewData] = useState(0)
+		 const { x, y } = useMousePosition(); //get mouse position 
+		 const hasMovedCursor = typeof x === "number" && typeof y === "number"; 
 
-  const [formValue, setFormValue] = useState("")
+		 const [scrollX, setScrollX] = useState(0) //pan
+		 const [scrollY, setScrollY] = useState(0)
 
- 
+		 const [diffX, setDiffX] = useState(0) //important! Diff between the mouse and the dot
+		 const [diffY, setDiffY] = useState(0) //important! Diff between the mouse and the dot
 
-  //Drag vars 
+		 const circle = useRef();
+		 const mainDiv = useRef();
 
- 
-  const [mySocket, setMySocket] = useState("")
-  const [dragSocket, setDragSocket] = useState("")
+		 const [circleStyle, setCircleStyle] = useState(null) //the point
 
-  const [moveableInitX, setMoveableInitX] = useState(0)
-  const [moveableInitY, setMoveableInitY] = useState(0)
+ // Drag vars 
 
-  const [moveables, setMoveables] = useState(-1)
+	  const [mySocket, setMySocket] = useState("")
+	  const [dragSocket, setDragSocket] = useState("")
 
-  const [positions, setPositions] = useState([])
+	  const [moveableInitX, setMoveableInitX] = useState(0)
+	  const [moveableInitY, setMoveableInitY] = useState(0)
 
-  //Zoom cars
+	  const [moveables, setMoveables] = useState(-1)
 
+	  const [positions, setPositions] = useState([])
 
- const [zoom, setZoom] = useState(1)
+ // Presumably obsolete or irrelevant vars
+ 	  const [sum, setSum] = useState(-1)
 
+ 	  const [response,setResponse] = useState('Loading...')
+ 	  const [passwords,setPasswords] = useState([])
+ 	  const [myVar,setMyVar] = useState("loading...")
 
- const [pinchScroll, setPinchScroll] = useState(false)
+ 	  const [test,setTest] = useState("TEST")
 
- const [pinchOffsetX, setPinchOffsetX] = useState(0)
- const [pinchOffsetY, setPinchOffsetY] = useState(0)
+ 	  const [value,setValue] = useState(null)
+ 	  const [newData,setNewData] = useState(0)
 
- const [isZoomingIn, setIsZoomingIn] = useState(null)
+ 	  const [pinchScroll, setPinchScroll] = useState(false) //determine whether we are getting a scroll from a pinch gesture
 
- const { x, y } = useMousePosition(); //get mouse position 
- const hasMovedCursor = typeof x === "number" && typeof y === "number"; //mouse position again
+ 	  const [pinchOffsetX, setPinchOffsetX] = useState(0)
+ 	  const [pinchOffsetY, setPinchOffsetY] = useState(0)
 
- const [cursorX, setCursorX] = useState(0) 
- const [cursorY, setCursorY] = useState(0)
+ 	  const [formValue, setFormValue] = useState("")
 
- const [startPanX, setStartPanX] = useState(null)
- const [startPanY, setStartPanY] = useState(null) //course "programming panning and zooming, 9:32" We have to update those vars only onPinch
-
- const [diffX, setDiffX] = useState(0) //important! Diff between the mouse and the dot
- const [diffY, setDiffY] = useState(0) //important! Diff between the mouse and the dot
-
- const ref = useRef(null);
- const mainDiv = useRef();
-
- const [circleStyle, setCircleStyle] = useState(null)
-
- //CANVAS
+ // Canvas (forJay: it's used to draw a line)
 
      const canvas = useRef();
      
@@ -104,12 +96,11 @@ function App () {
  
 
      useEffect(() => {
-
-      setCircleStyle({ //the point
+      // forJay: the point	
+      setCircleStyle({ 
       position:'absolute',
-      //set scale the opposite to zoom like 0.something
-      top: (y + cursorY),
-      left: (x + cursorX),
+      top: (y + scrollY),
+      left: (x + scrollX),
       padding:0,
       margin:0,
       display:"inline-block",
@@ -117,7 +108,7 @@ function App () {
       borderRadius: "50%",
       width:0.5,
       height:0.5,
-    })
+   	 })
 
 
       
@@ -135,321 +126,252 @@ function App () {
 
 
 
-     }, [x,y, zoom, cursorX, cursorY]);
+     }, [x,y, zoom, scrollX, scrollY]);
 
-     useEffect(() => {
-       drawLine({ x: x, y: y-35, x1: ref.current.getBoundingClientRect().x, y1: ref.current.getBoundingClientRect().y-35 });
+     useEffect(() => { 
 
-       console.log("Diff from DrawLine: ", diffY)
+       // forJay: drawing a line between the cursor and the point: 
+       drawLine({ x: x, y: y, x1: circle.current.getBoundingClientRect().x, y1: circle.current.getBoundingClientRect().y });
 
-     }, [zoom,x,y, cursorX, cursorY]);
+       // console.log("Diff from DrawLine: ", diffY)
 
+     }, [zoom,x,y, scrollX, scrollY]);
 
+ // forJay: setting the difference between our point and the cursor
 
- useEffect(() => {  
-     console.log('Point diff X', ref.current ? ref.current.getBoundingClientRect().x-x : 0);
-     console.log('Point diff Y', ref.current ? ref.current.getBoundingClientRect().y-y : 0);
+	 useEffect(() => {  
 
-     setDiffX(  (ref.current.getBoundingClientRect().x) - x   ) // /zoom?
-     setDiffY(  ( (ref.current.getBoundingClientRect().y-35) - (y-35)  )   )
+	     // console.log('Point diff X', circle.current ? circle.current.getBoundingClientRect().x-x : 0);
+	     // console.log('Point diff Y', circle.current ? circle.current.getBoundingClientRect().y-y : 0);
 
+	     setDiffX(  circle.current ? circle.current.getBoundingClientRect().x-x : 0   ) 
+	     setDiffY(  circle.current ? circle.current.getBoundingClientRect().y-y : 0   )
 
 
+	   }, [zoom]);
 
-     // setPinchOffsetY(ref.current.getBoundingClientRect().height)
-     // setPinchOffsetX(ref.current.getBoundingClientRect().width)
+ // forJay: the code below is not related to the problem 
 
-     // ref.current.offsetWidth
 
-   }, [zoom]);
+					  useEffect(() => {
 
+					    disableBodyScroll(document.querySelector(".App"))
 
+					    socket.on('remoteAddMoveables', (moveables, moveableInitX, moveableInitY) => {
 
+					      setMoveableInitX(moveableInitX)
+					      setMoveableInitY(moveableInitY)
+					       setMoveables(moveables) 
 
 
-  useEffect(() => {
+					    });
 
-    disableBodyScroll(document.querySelector(".App"))
+					    socket.on('socketInfo', (data) => {
+					          // we get settings data and can do something with it
+					          // console.log("SOCKET INFO ",data)
+					          // setSum(data)
 
-    socket.on('remoteAddMoveables', (moveables, moveableInitX, moveableInitY) => {
+					          setMySocket(data)
 
-      setMoveableInitX(moveableInitX)
-      setMoveableInitY(moveableInitY)
-       setMoveables(moveables) 
+					        });
 
 
-    });
+					    socket.emit("read")
 
-    socket.on('socketInfo', (data) => {
-          // we get settings data and can do something with it
-          // console.log("SOCKET INFO ",data)
-          // setSum(data)
+					    socket.on('readResponse', (howManyMoveables, positions) => {
 
-          setMySocket(data)
+					          
+					        //for some strange reason it doesn't work as intended. Inspect!
 
-        });
+					        });
 
+					    
 
-    socket.emit("read")
+					    socket.on('new-remote-operations', (data: number, howManyMoveables, positions) => {
 
-    socket.on('readResponse', (howManyMoveables, positions) => {
+					          setPositions(positions)  
+					          setMoveables(howManyMoveables) 
+					        //  console.log("howManyMoveables", howManyMoveables)  
+					          setSum(data)
 
-          
-        //for some strange reason it doesn't work as intended. Inspect!
+					        });
 
-        });
+					  }, []);
 
-    
+					  function handleClick() {
 
-    socket.on('new-remote-operations', (data: number, howManyMoveables, positions) => {
+					  
+					  }
 
-          setPositions(positions)  
-          setMoveables(howManyMoveables) 
-        //  console.log("howManyMoveables", howManyMoveables)  
-          setSum(data)
+					  function handleSubmit(e : any) {
+					    e.preventDefault();
 
-        });
 
-  }, []);
+					    
+					    if (Number.isNaN(newData) == true){
+					      alert("wrong number!")
 
-  function handleClick() {
+					    } else {
 
-  
-  }
 
-  function handleSubmit(e : any) {
-    e.preventDefault();
+					     socket.emit("read")
+					     socket.on('readResponse', (data: number) => {
 
+					          
+					           socket.emit("new-operations", data+newData)
+					           
+					           setSum(data)
 
-    
-    if (Number.isNaN(newData) == true){
-      alert("wrong number!")
+					           socket.off('readResponse')
 
-    } else {
 
 
-     socket.emit("read")
-     socket.on('readResponse', (data: number) => {
+					         });
 
-          
-           socket.emit("new-operations", data+newData)
-           
-           setSum(data)
 
-           socket.off('readResponse')
+					     // setMyVar((prev) => (prev+newData) )
+					   }
 
+					    setFormValue("") 
+					    
 
 
-         });
 
+					  }
 
-     // setMyVar((prev) => (prev+newData) )
-   }
 
-    setFormValue("") 
-    
+					  function handleChange(e: any) {
 
+					    setNewData(Number(e.target.value)) 
+					    setFormValue(e.target.value) 
+					  
 
 
-  }
 
+					    
 
-  function handleChange(e: any) {
+					  }
 
-    setNewData(Number(e.target.value)) 
-    setFormValue(e.target.value) 
-  
+					  function handleMoveableAddition (e) {
 
 
+					    //console.log("E!", e)
+					   
+					    setMoveableInitX(e.clientX) //clientX or screenX
+					    setMoveableInitY(e.clientY-33)
 
-    
 
-  }
+					    setMoveables(prev => prev+1)
+					    socket.emit("addMoveables", moveables+1, e.clientX, e.clientY-33) //WE HAVE TO MAKE IT BETTER
+					     
 
-  function handleMoveableAddition (e) {
 
+					  
 
-    //console.log("E!", e)
-   
-    setMoveableInitX(e.clientX) //clientX or screenX
-    setMoveableInitY(e.clientY-33)
 
-
-    setMoveables(prev => prev+1)
-    socket.emit("addMoveables", moveables+1, e.clientX, e.clientY-33) //WE HAVE TO MAKE IT BETTER
-     
-
-
-  
-
-
-}
-  document.ondblclick = (e) => handleMoveableAddition(e);
-
-
- 
- 
-
+						}
+						  document.ondblclick = (e) => handleMoveableAddition(e);
 
 
   return (
 
-
-
-   
-
-
     <div ref={mainDiv} className="App"  style={{userSelect: 'none', overflow: 'visible'}}>
 
+    {/*Info floating div*/} 
 
-    <div style= {{zIndex: '1',  pointerEvents: 'none', position: 'absolute', height: '100vh'}}> 
-    Scroll X {cursorX} <br/>
-    Scroll Y {cursorY} <br/>
-    <canvas ref={canvas}></canvas>
+	    <div style= {{zIndex: '2',  pointerEvents: 'none', position: 'absolute', height: '100vh'}}> 
 
+	        Zoom {zoom}  <br/>
+	        {hasMovedCursor ? (`Your cursor is at ${x}, ${y}.`) : "Move your mouse around."}  <br/>
+	        Diff X {diffX}   <br/>     
+	        Diff Y {diffY}   <br/>  	
+	        Scroll X {scrollX} <br/>
+	        Scroll Y {scrollY} <br/>
 
+	     </div>   
 
-    
-{/*   Offset X {pinchOffsetX} <br/>
-   Offset Y {pinchOffsetY} <br/>*/}
-{/*   Zoom {zoom}  <br/>
-   {hasMovedCursor
-           ? (`Your cursor is at ${x}, ${y}.`)
-           : "Move your mouse around."} <br/>
+	{/*Canvas floating div (it's used to draw a line)*/} 
 
+	    <div style= {{zIndex: '1',  pointerEvents: 'none', position: 'absolute', height: '100vh'}}> 
 
+	    <canvas ref={canvas}></canvas>
 
+	  	</div>
 
-    Diff X {diffX}        
-    Diff Y {diffY}    
-
-    <br/>    
-
-   Scroll X {cursorX} <br/>
-   Scroll Y {cursorY} <br/>
-
- */}
-
-{/*   <div style={circleStyle}>
-   </div>
-*/}
-
-
-
-
-   </div>
-
-
+	{/*The actual content div*/} 
  
-    <InfiniteViewer
-     
-        className="viewer"
-        margin={1}
-        threshold={1}
-        rangeX={[100000000, 100000000]}
-        rangeY={[100000000, 100000000]}
+	    <InfiniteViewer
+	     
+		        className="viewer"
+		        margin={1}
+		        threshold={1}
+		        rangeX={[100000000, 100000000]}
+		        rangeY={[100000000, 100000000]}
+		        pinchThreshold = {1}
+		        onScroll = {e => {
 
-        pinchThreshold = {1}
+		        {/*console.log("Scroll event",e)*/}
 
- 
-        onScroll = {e => {
+		                  setScrollX(e.scrollLeft)
+		                  setScrollY(e.scrollTop)
 
-        console.log("Scroll event",e)
+		        }}
 
+		        onPinch={e => {
 
-                  setCursorX(e.scrollLeft)
-                  setCursorY(e.scrollTop)
+		          if (e.zoom < 1  ) {
+		           {/* console.log('zooming out');*/}
 
+		            setIsZoomingIn(false)
 
+		          } else { 
 
+		            {/*console.log('zooming in') ;*/}
 
+		            setIsZoomingIn(true) 
+		     
+		          }
 
+		         setZoom( prev => prev* e.zoom*e.scale )  
 
-        }}
+		        }}
 
-        onPinch={e => {
-
-
-        {/*  console.log("PINCH E", e.inputEvent.clientX)*/}
-
-          if (e.zoom < 1  ) {
-            console.log('zooming out');
-
-            setIsZoomingIn(false)
-
-
-
-
-
-
-          } else { 
-
-            console.log('zooming in') ;
-
-
-
-            setIsZoomingIn(true) 
-     
-
-          }
-
-
-    
-         setZoom((prev) => (prev* e.zoom*e.scale) 
+	        >
 
 
 
 
+	        <div  style={{height: '100vh'}} >
 
-          )  
+	        	<div style={{transform:'scale('+zoom+')' +  'translateX('+ diffX +'px)' + 'translateY('+ diffY +'px)' , backgroundColor: "white", height: '100vh'}}>
 
+	        		<div ref={circle} style={circleStyle}> {/*forJay: the point*/}
+	        		</div>
 
+	        		{/*forJay: moveable content*/} 
 
+	        		{ moveables > -1 && ( [...Array(moveables)].map((e, i) =>  
+	        			
+	        			<span style={{position: 'absolute', top:0, left: 0}}> 
 
+	        				<Moveable  
+	        				socket={socket} 
+	        				mySocket={mySocket} 
+	        				id = {i}  
+	        				x={moveableInitX} 
+	        				y={moveableInitY}> 
+	        				</Moveable>
 
-        }}
+	    				</span>) 
 
-        >
+	        		)}
 
+	        	</div>
 
+	        </div>
 
-
-        <div  style={{height: '100vh'}} >
-
-
-
-        <div   style={{transform:'scale('+zoom+')' +  'translateX('+ diffX +'px)' + 'translateY('+ diffY +'px)' , backgroundColor: "white", height: '100vh'}}>
-
-        <div ref={ref} style={circleStyle}>
-        </div>
-
-
-       
-
-
-
-        { moveables > -1 && ( [...Array(moveables)].map((e, i) =>  <span style={{position: 'absolute', top:0, left: 0}}>  <Moveable  socket={socket} mySocket={mySocket} id = {i}  x={moveableInitX} y={moveableInitY}>  </Moveable> </span>) )}
-
-        {/*<div style={{transform:  'translateX('+ pinchOffsetX/2 +'px)' + 'translateY('+ pinchOffsetY/2 +'px)'}}> CENTER </div>*/}
-     
-
-        </div>
-
-        </div>
-
-    </InfiniteViewer>
-    
-
-
-  
-
-  
-
-
-
+	    </InfiniteViewer>
    
-  
-
     </div>
   );
 
--- index.js
+++ index.js
@@ -5,210 +5,173 @@ const generatePassword = require('password-generator');
 const { MongoClient } = require("mongodb");
 const url = "mongodb+srv://zenberry:mongoPass8@cluster0-xmkeu.mongodb.net/sample_restaurants?retryWrites=true&w=majority";
 const client = new MongoClient(url);
- 
- // The database to use
- const dbName = "test";
+
+// The database to use
+const dbName = "test";
 
 const app = express();
 
-
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
-
-
 
 var initial = true
 var serverData = null
 var howManyMoveables = 0
 
-var positions =  []
-// var positions =  [{id:0, x:50, y:50}, {id:1, x:100, y:100}, {id:2, x:200, y:200}, {id:3, x:300, y:300}, {id:4, x:400, y:400}, {id:5, x:500, y:500} ]
-  
-
+var positions = []
 
 io.on("connection", function(socket) {
 
-	var socketID = socket.id	
+    var socketID = socket.id
 
-  socket.on("typing", (value, atWhichSocket, id, largestLine, rows) => {
+    socket.on("typing", (value, atWhichSocket, id, largestLine, rows) => {
 
-    positions[id].value = value;
-    positions[id].largestLine = largestLine;
-    positions[id].rows=rows;
+            positions[id].value = value;
+            positions[id].largestLine = largestLine;
+            positions[id].rows = rows;
 
-    console.log(positions)
+            console.log(positions)
 
-    io.emit("remote-typing", value, atWhichSocket, id, largestLine, rows)
+            io.emit("remote-typing", value, atWhichSocket, id, largestLine, rows)
 
-   // console.log("VALUE", value, "AT WHICH SOCKET", atWhichSocket, "ID", id)
+            // console.log("VALUE", value, "AT WHICH SOCKET", atWhichSocket, "ID", id)
 
-  }
-
+        }
 
     )
 
+    // COMMENTED OUT AT 3 AUG console.log("SOCKET CONNECTED. ID = ", socket.id)
 
-  
-
-  // // COMMENTED OUT AT 3 AUG console.log("SOCKET CONNECTED. ID = ", socket.id)
-  
-  io.to(socket.id).emit("socketInfo", socket.id) //emitting socket id to different clients
+    io.to(socket.id).emit("socketInfo", socket.id) //emitting socket id to different clients
 
 
-  // if (initial === true) { //finance stuff 
+    // if (initial === true) { //finance stuff 
+
+    //  MongoClient.connect(url, {useUnifiedTopology: true}, { useNewUrlParser: true }, function(err, db) { 
+    //    if (err) throw err;
+    //    var dbo = db.db("finance");
+    //    var myquery = { Name: "cashTest1" };
+    //    // var newvalues = { $set: {Note : req.body.title} };
 
 
+    //  dbo.collection("finance").find(myquery).toArray(function(err, result) {
+    //    if (err) throw err;
+    //    // COMMENTED OUT AT 3 AUG console.log(result[0].Value);
 
-  // 	MongoClient.connect(url, {useUnifiedTopology: true}, { useNewUrlParser: true }, function(err, db) { 
-  // 	  if (err) throw err;
-  // 	  var dbo = db.db("finance");
-  // 	  var myquery = { Name: "cashTest1" };
-  // 	  // var newvalues = { $set: {Note : req.body.title} };
+    //    serverData = Number(result[0].Value)
+    //    io.emit("new-remote-operations", serverData, positions);
 
+    //    // COMMENTED OUT AT 3 AUG console.log(result[0].Name);
+    //    db.close();
 
-  // 	dbo.collection("finance").find(myquery).toArray(function(err, result) {
-  // 	  if (err) throw err;
-  // 	  // // COMMENTED OUT AT 3 AUG console.log(result[0].Value);
+    //  });
 
-  // 	  serverData = Number(result[0].Value)
-  // 	  io.emit("new-remote-operations", serverData, positions);
-
-  // 	  // // COMMENTED OUT AT 3 AUG console.log(result[0].Name);
-  // 	  db.close();
-  	  
-  // 	});
-
-  // 	});
-
-    
-  //   initial = false
-
-  // } else {
-  //   io.emit("new-remote-operations", serverData, positions);
-  // }
-
-   io.emit("new-remote-operations", serverData, positions);
-
-  socket.on("drag", function(data, moveableId) { 
-
-    positions[moveableId].x = data.x
-    positions[moveableId].y = data.y
+    //  });
 
 
+    //   initial = false
 
-  	// // COMMENTED OUT AT 3 AUG console.log(data)
-  	io.emit("remoteDrag", data, socketID, moveableId);
+    // } else {
+    //   io.emit("new-remote-operations", serverData, positions);
+    // }
 
-  })
+    io.emit("new-remote-operations", serverData, positions);
 
-  socket.on("new-operations", function(data) {
+    socket.on("drag", function(data, moveableId) {
 
-    io.emit("new-remote-operations", data); //sending new data back to client
-    serverData = data
+        positions[moveableId].x = data.x
+        positions[moveableId].y = data.y
 
+        // COMMENTED OUT AT 3 AUG console.log(data)
+        io.emit("remoteDrag", data, socketID, moveableId);
 
-    MongoClient.connect(url, {useUnifiedTopology: true}, { useNewUrlParser: true }, function(err, db) {
-    	//sending new data back to the DB
+    })
 
-	      if (err) throw err;
-	      var dbo = db.db("finance");
-	      var myquery = { Name: "cashTest1" };
+    socket.on("new-operations", function(data) {
 
-	      var newvalues = { $set: {Value: serverData} };
-
-
-	     
-
-
-	      dbo.collection("finance").updateOne(myquery, newvalues, function(err, res) {
-	        if (err) throw err;
-	        // // COMMENTED OUT AT 3 AUG console.log("1 document updated");
-
-	        db.close();
-	        
-	      });
-
-     
-
-    }); //MongoClient.connect END
+        io.emit("new-remote-operations", data); //sending new data back to client
+        serverData = data
 
 
-    
-  }); //socket.on("new-operations" END
+        MongoClient.connect(url, { useUnifiedTopology: true }, { useNewUrlParser: true }, function(err, db) {
+            //sending new data back to the DB
 
-  socket.on("addMoveables", function(moveables, moveableInitX, moveableInitY) { //adding new moveables
+            if (err) throw err;
+            var dbo = db.db("finance");
+            var myquery = { Name: "cashTest1" };
 
-    howManyMoveables=moveables
+            var newvalues = { $set: { Value: serverData } };
 
-   // console.log(moveables)
+            dbo.collection("finance").updateOne(myquery, newvalues, function(err, res) {
+                if (err) throw err;
+                // COMMENTED OUT AT 3 AUG console.log("1 document updated");
 
-    // positions[moveables-1].x = moveableInitX //saving just-added moveable position to the server
-    // positions[moveables-1].y = moveableInitY
+                db.close();
 
-    positions.push({id:moveables-1, x:moveableInitX, y:moveableInitY, largestLine: 0, rows: 1}) //PAUSED HERE 3 AUG! SEE YA!
+            });
+
+        }); //MongoClient.connect END
+
+    }); //socket.on("new-operations" END
+
+    socket.on("addMoveables", function(moveables, moveableInitX, moveableInitY) { //adding new moveables
+
+        howManyMoveables = moveables
+
+        // console.log(moveables)
+
+        // positions[moveables-1].x = moveableInitX //saving just-added moveable position to the server
+        // positions[moveables-1].y = moveableInitY
+
+        positions.push({ id: moveables - 1, x: moveableInitX, y: moveableInitY, largestLine: 0, rows: 1 }) //PAUSED HERE 3 AUG! SEE YA!
+
+        // COMMENTED OUT AT 3 AUG console.log("ADD MOVEABLES EMITTED")
+        // COMMENTED OUT AT 3 AUG console.log("HOW MANY MOV. FROM ADD MOV.",howManyMoveables)
+
+        io.emit("remoteAddMoveables", moveables, moveableInitX, moveableInitY);
 
 
 
+        // // COMMENTED OUT AT 3 AUG console.log("MOVEABLES", moveables, moveableInitX, moveableInitY)
 
+    })
 
+    socket.on("read", function() {
 
-    // COMMENTED OUT AT 3 AUG console.log("ADD MOVEABLES EMITTED")
-    // COMMENTED OUT AT 3 AUG console.log("HOW MANY MOV. FROM ADD MOV.",howManyMoveables)
+        // COMMENTED OUT AT 3 AUG console.log("READ EMITTED")
 
-  	io.emit("remoteAddMoveables", moveables, moveableInitX, moveableInitY);
+        // COMMENTED OUT AT 3 AUG console.log("HOW MANY MOV. FROM READ",howManyMoveables)
 
+        io.emit("readResponse", serverData, howManyMoveables, positions);
+        io.emit("new-remote-operations", serverData, howManyMoveables, positions);
+        io.emit("TEST", positions);
 
+    });
 
-  	// // COMMENTED OUT AT 3 AUG console.log("MOVEABLES", moveables, moveableInitX, moveableInitY)
-
-  })
-
-  socket.on("read", function() {
-
-    // // COMMENTED OUT AT 3 AUG console.log("READ EMITTED")
-
-    // COMMENTED OUT AT 3 AUG console.log("HOW MANY MOV. FROM READ",howManyMoveables)
-
-
-
-    io.emit("readResponse", serverData, howManyMoveables, positions);
-    io.emit("new-remote-operations", serverData, howManyMoveables, positions);
-    io.emit("TEST", positions);
-
-    
-  });
-
-  });
-
+});
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.use(function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Headers", "X-Requested-With");
-        res.header("Access-Control-Allow-Headers", "Content-Type");
-        res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-        next();
-    });
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
 
 // Put all API endpoints under '/api'
 
-
-app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.json()); // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
-
-
-
-
 
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
-
 
 
 const port = process.env.PORT || 5000;
